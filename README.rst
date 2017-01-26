5729eef18c0b1a8a1fc8c0f02904f6680ca3a497
chdorner <christof@chdorner.com>
2017-01-26 18:17:07 +0100

    Add celery beat configuration
    This is all that's necessary to get a basic celery beat process up and
    running with the correct definition of all periodic tasks that we
    currently use in `h`.

--- README.rst
+++ README.rst
@@ -1,2 +1,8 @@
 Periodic tasks for h
 ====================
+
+This contains the necessary configuration for running celery beat to schedule
+periodic tasks that the celery workers in `h` will pick up.
+
+While this will be used as a docker container on production/staging, for
+development purposes this needs to run manually with `celery -A hperiodic beat`
---
+++ hperiodic.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""
+Celery configuration for a separata beat process.
+"""
+
+from __future__ import absolute_import
+
+from datetime import timedelta
+
+from celery import Celery
+
+celery = Celery('h')
+celery.conf.update(
+    CELERYBEAT_SCHEDULE={
+        'purge-deleted-annotations': {
+            'task': 'h.tasks.cleanup.purge_deleted_annotations',
+            'schedule': timedelta(hours=1)
+        },
+        'purge-expired-authtickets': {
+            'task': 'h.tasks.cleanup.purge_expired_auth_tickets',
+            'schedule': timedelta(hours=1)
+        },
+        'purge-expired-tokens': {
+            'task': 'h.tasks.cleanup.purge_expired_tokens',
+            'schedule': timedelta(hours=1)
+        },
+        'purge-removed-features': {
+            'task': 'h.tasks.cleanup.purge_removed_features',
+            'schedule': timedelta(hours=6)
+        },
+    },
+    CELERY_TASK_SERIALIZER='json',
+)
