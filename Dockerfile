fc634b269f996ad5fd852838c8b09b3c2f5aff4d
Robert Knight <robertknight@gmail.com>
2018-05-22 15:31:16 +0100

    Support Amazon SQS as a message broker
     - Configure the AWS region and SQS queue name prefix when using SQS as
       the broker.  This configuration needs to match h's configuration when
       using SQS.
    
     - Add pycurl and boto3 dependencies required by Kombu's SQS support.
       The recommended way to do this is to use the `celery[sqs]` bundle but
       that includes the wrong version of boto in Celery 4.1.x. This is fixed
       in the unreleased Celery 4.2.x.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine:3.7
 MAINTAINER Hypothes.is Project and contributors
 
 # Install system and runtime dependencies.
-RUN apk add --no-cache python2 py2-pip
+RUN apk add --no-cache libcurl python2 py2-pip
 
 # Create the hypothesis user, group, home directory and package directory.
 RUN addgroup -S hypothesis && adduser -S -G hypothesis -h /var/lib/h-periodic hypothesis
@@ -12,7 +12,11 @@ COPY README.rst requirements.txt supervisord.conf start.sh hperiodic.py healthch
 
 # Install build deps, build then clean up.
 RUN apk add --no-cache --virtual build-deps \
+    build-base \
     git \
+    curl-dev \
+    openssl-dev \
+    python-dev \
     && pip install --no-cache-dir -U pip supervisor \
     && pip install --no-cache-dir -r requirements.txt \
     && apk del build-deps
--- hperiodic.py
+++ hperiodic.py
@@ -7,11 +7,31 @@ Celery configuration for a separate beat process.
 from __future__ import absolute_import
 
 from datetime import timedelta
+import os
 
 from celery import Celery
 
+# This can be imported on any system but properties and methods can only be
+# accessed when running on an actual EC2 instance.
+from ec2_metadata import ec2_metadata
+
+# Configure the broker transport. These settings need to match the
+# configuration of the Celery worker in 'h' which will actually execute the
+# tasks.
+broker_url = os.getenv('BROKER_URL','')
+broker_transport_options = {}
+if broker_url.startswith('sqs://'):
+    broker_transport_options = {
+        # Use SQS from the same region as the EC2 instance on which h-periodic
+        # is running.
+        'region': ec2_metadata.region,
+        # Use the same Celery queue name prefix that 'h' uses.
+        'queue_name_prefix': 'hypothesis-h-',
+    }
+
 celery = Celery('h')
 celery.conf.update(
+    broker_transport_options=broker_transport_options,
     beat_schedule={
         'purge-deleted-annotations': {
             'task': 'h.tasks.cleanup.purge_deleted_annotations',
