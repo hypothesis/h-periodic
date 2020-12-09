c006bfb3e7f747147cb15e9aeb82118acdf6bc72
Jon Betts <jon.betts@hypothes.is>
2020-12-09 14:04:47 +0000

    Separate and give much more detailed information in our status end-point
    This now will tell you all the failures, rather than just the first one.
    This also nests in the `h` to make room for `checkmate` coming soon.

--- h_periodic/views/status.py
+++ h_periodic/views/status.py
@@ -2,32 +2,69 @@ import os
 
 import kombu
 import psutil
-from pyramid.view import exception_view_config, view_config, view_defaults
+from kombu.exceptions import KombuError
+from pyramid.view import view_config, view_defaults
 
 
 @view_defaults(route_name="status", renderer="json", request_method="GET")
-class StatusViews:
-    def __init__(self, context, request):
-        self.context = context
+class StatusView:
+    H_PID_FILE = "celerybeat.pid"
+    H_BROKER_VAR = "BROKER_URL"
+
+    def __init__(self, _context, request):
         self.request = request
 
-    @staticmethod
     @view_config()
-    def status():
-        # First check that the `celery beat` process is running.
-        if not psutil.pid_exists(int(open("celerybeat.pid", "r").read())):
-            raise RuntimeError("It looks like `celery beat` isn't running")
+    def status(self):
+        data = {"h": self._get_status(self.H_PID_FILE, self.H_BROKER_VAR)}
 
-        # Next check that we can connect to the message broker at BROKER_URL.
-        connection = kombu.Connection(os.environ["BROKER_URL"])
-        try:
-            connection.connect()
-        finally:
-            connection.close()
+        data["status"] = self._summarise(data["h"]["status"] == "ok")
 
-        return {"status": "ok"}
+        self.request.response.status_int = 200 if data["status"] == "ok" else 500
 
-    @exception_view_config(Exception)
-    def status_exception(self):
-        self.request.response.status_int = 500
-        return {"status": "error", "reason": repr(self.context)}
+        return data
+
+    @classmethod
+    def _get_status(cls, pid_file, broker_var):
+        pid_ok = cls._check_pid_file(pid_file)
+        connection_ok = cls._check_broker(broker_var)
+
+        return {
+            "beat": cls._summarise(pid_ok),
+            "connection": cls._summarise(connection_ok),
+            "status": cls._summarise(pid_ok, connection_ok),
+        }
+
+    @classmethod
+    def _check_pid_file(cls, pid_file):
+        if not os.path.exists(pid_file):
+            return False
+
+        return psutil.pid_exists(int(open(pid_file, "r").read()))
+
+    @classmethod
+    def _check_broker(cls, broker_var):
+        broker_url = os.environ.get(broker_var)
+
+        if broker_url:
+            connection = kombu.Connection(broker_url)
+
+            try:
+                connection.connect()
+                return True
+            except KombuError:
+                pass
+            finally:
+                connection.close()
+
+        return False
+
+    @staticmethod
+    def _summarise(*values):
+        """Create a textual summary of a series of truthy values."""
+
+        for value in values:
+            if not value:
+                return "down"
+
+        return "ok"
