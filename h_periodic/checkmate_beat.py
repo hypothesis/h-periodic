466f67c3a67b94e73a4dd0f8ef8d6ecb4dedca31
Jon Betts <jon.betts@hypothes.is>
2020-12-10 13:06:28 +0000

    Fix the settings on the celery exchange/queue to match checkmate
    The `h` beat works by happening to line up with the default, but checkmate
    is different, so it doesn't work.

--- h_periodic/checkmate_beat.py
+++ h_periodic/checkmate_beat.py
@@ -2,9 +2,19 @@
 from datetime import timedelta
 
 from celery import Celery
+from kombu import Exchange, Queue
 
 celery = Celery("checkmate")
 celery.conf.update(
+    task_queues=[
+        Queue(
+            "celery",
+            # We don't care if the messages are lost if the broker restarts
+            durable=False,
+            routing_key="celery",
+            exchange=Exchange("celery", type="direct", durable=False),
+        ),
+    ],
     beat_schedule_filename="checkmate-celerybeat-schedule",
     beat_schedule={
         "sync-blocklist": {
