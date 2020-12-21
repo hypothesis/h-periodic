315c5fe9569a0e0470b146eefcff372ed40f3c86
Sean Hammond <seanh@users.noreply.github.com>
2020-12-21 13:50:12 +0000

    Remove the status endpoint
    This means that Pyramid is no longer needed, since the Pyramid app only
    served the status endpoint.
    
    The status endpoint doesn't do anything, for a few reasons:
    
    1. Elastic Load Balancing health checks just send an HTTP request to the
       configured health check path and consider the server healthy if it
       returns 200 OK, unhealthy otherwise.
    
       The JSON body with the `status` field and individual status
       information about each beat process and its connection to RabbitMQ is
       *not* used by Elastic Load Balancing.
    
       The JSON body can't be used by humans either, since h-periodic is not
       exposed to the internet. Nor would we want to expose h-periodic's
       status endpoint to the internet since hitting it consumes a
       non-trivial amount of resources.
    
       There's just no point in having all this code, and there's a
       maintenance cost to it, for example if I want to disable the h beat
       on h-periodic-qa I'll have to update this status code which currently
       assumes that both beats are always wanted all the time.
    
       All a status endpoint should do is return 200 or 500, no body, since
       that's all that actually matters.
    
       If further information about the app's status is wanted for humans
       the status endpoint's responses aren't the place for that since they
       aren't accessible. Logging might be a better place.
    
       See: https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/using-features.healthstatus.html#using-features.healthstatus.understanding
    
    2. The endpoint is coded to return 200 OK if _either_ of the beats (h or
       checkmate) is ok, but with `"status": "degraded"` in the JSON body if
       one of the beats is broken. Elastic Load Balancing would ignore this
       JSON body and just look at the 200 OK HTTP status and assume the app
       is healthy. _Both_ of the beats would have to be down at the same
       time in order for the status endpoint to return 500 and trigger a
       health alert. There is no concept of "degraded" in Elastic Load
       Balancing health checks, it's just either 200 OK or other.
    
    3. The above points are both moot anyway because
       [health check URLs aren't supported at all for single-instance apps](https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/using-features.healthstatus.html#monitoring-basic-healthcheck-singleinstance)
       like h-periodic: <q>In a single instance or worker tier environment,
       Elastic Beanstalk determines the instance's health by monitoring its
       Amazon EC2 instance status. Elastic Load Balancing health settings,
       including HTTP health check URLs, cannot be used in these environment
       types.</q>
    
       h-periodic uses EC2 instance status checks instead:
       https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/monitoring-system-instance-status-check.html
    
       I don't think these EC2 instance status checks allow us to plug into
       them in any way and change the status if the beat process isn't
       running or the RabbitMQ connection isn't working, like we're trying
       to do in `status.py`. I think if we want an alarm to go off if
       h-periodic isn't working, we're going to have to implement that in
       other ways:
    
       * The annotation sync queue reports metrics to New Relic and has New
         Relic alarms for if these metrics drop
       * Checkmate [needs to do the same](https://github.com/hypothesis/checkmate/issues/56)

--- h_periodic/app.py
+++
@@ -1,9 +0,0 @@
-from pyramid.config import Configurator
-
-
-def create_app(_global_config, **settings):  # pragma: no cover
-    with Configurator(settings=settings) as config:
-        config.add_route("status", "/_status")
-        config.scan("h_periodic.views")
-
-    return config.make_wsgi_app()
