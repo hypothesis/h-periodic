948fc1815997c2f1fd54bebe542380920b9932b1
Sean Hammond <seanh@users.noreply.github.com>
2025-05-01 17:44:06 +0100

    Enable Sentry's "releases" feature
    The releases feature is useful in itself, and this should also stop the
    "discarded session update because of missing release" messages that
    `sentry_sdk` is logging all the time.

--- h_periodic/__init__.py
+++ h_periodic/__init__.py
@@ -1,4 +1,11 @@
 import sentry_sdk
 from sentry_sdk.integrations.celery import CeleryIntegration
 
-sentry_sdk.init(integrations=[CeleryIntegration()])
+from h_periodic._version import get_version
+
+sentry_sdk.init(
+    integrations=[CeleryIntegration()],
+    # Enable Sentry's "Releases" feature, see:
+    # https://docs.sentry.io/platforms/python/configuration/options/#release
+    release=get_version(),
+)
